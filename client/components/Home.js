3ca7cb879b32072bb25fed8058154396fbdae4c2
ahaha <101982272+Git4RJS@users.noreply.github.com>
2023-06-20 15:30:03 +0700

    Update Home.js

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,75 +1,74 @@
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import { Card } from 'react-native-paper';
-const Custom = ({
- screenName
-}) => {
+import {LogOut} from "./LogOut"
+const Custom = ({ screenName }) => {
   return (
-
-        <Card style={styles.list}>
-          <Text>{screenName}</Text>
-        </Card>
+    <Card style={styles.list}>
+      <Text>{screenName}</Text>
+    </Card>
   );
 };
 
-export default LoginScreen = (props, { navigation }) => {
 
+export default HomeScreen = (props,) => {
   return (
-  <View>
-
-     <Text style={styles.txt}>Account</Text>
-           <TouchableOpacity onPress={() => props.navigation.navigate('Edit Profile')}>
-           <Custom screenName="Edit Profile"/>
-         </TouchableOpacity>
-          <TouchableOpacity onPress={() => props.navigation.navigate('Login')}>
-           <Custom screenName="Log Out"/>
-         </TouchableOpacity>
-  <Text style={styles.txt}>List of requests</Text>
-           <TouchableOpacity onPress={() => props.navigation.navigate('Sent Requests')}>
-           <Custom screenName="Sent Requests"/>
-         </TouchableOpacity>
-       <TouchableOpacity onPress={() => props.navigation.navigate('Received Requests')}>
-           <Custom screenName="Received Requests"/>
-         </TouchableOpacity>
-    <Text style={styles.txt}>Family</Text>
-           <TouchableOpacity onPress={() => props.navigation.navigate('Family')}>
-           <Custom screenName="Add new member"/>
-         </TouchableOpacity>
-           <TouchableOpacity onPress={() => props.navigation.navigate('View family member\'s profiles')}>
-           <Custom screenName="View family member's profiles"/>
-         </TouchableOpacity>
-           <Text style={styles.txt}>Record</Text>
-           <TouchableOpacity onPress={() => props.navigation.navigate('My Record')}>
-           <Custom screenName="My Records"/>
-         </TouchableOpacity>
-       <TouchableOpacity onPress={() => props.navigation.navigate('Received Records')}>
-           <Custom screenName="Received Records"/>
-         </TouchableOpacity>
-   
-      </View>
-    
+    <View>
+      <Text style={styles.txt}>Account</Text>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Edit Profile')}>
+        <Custom screenName="Edit Profile" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => LogOut()}>
+        <Custom screenName="Log Out" />
+      </TouchableOpacity>
+      <Text style={styles.txt}>List of requests</Text>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Sent Requests')}>
+        <Custom screenName="Sent Requests" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Received Requests')}>
+        <Custom screenName="Download My Record Requests" />
+      </TouchableOpacity>
+            <TouchableOpacity
+        onPress={() => props.navigation.navigate('Relationship Requests')}>
+        <Custom screenName="Set Relationship Requests" />
+      </TouchableOpacity>
+      <Text style={styles.txt}>Family</Text>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Add family member')}>
+        <Custom screenName="Add new member" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          props.navigation.navigate("View family member's profiles")
+        }>
+        <Custom screenName="View family member's profiles" />
+      </TouchableOpacity>
+      <Text style={styles.txt}>Record</Text>
+      <TouchableOpacity onPress={() => props.navigation.navigate('My Record')}>
+        <Custom screenName="My Records" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('Received Records')}>
+        <Custom screenName="Received Records" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
- txt:{
-   padding: 5,
-   fontSize:15,
-
- },
-  list:{
-     marginTop:0,
-     height:50,
-     alignItems: 'center',
-     marginBottom:2,
-     width: '100%',
+  txt: {
+    padding: 5,
+    fontSize: 15,
+  },
+  list: {
+    marginTop: 0,
+    height: 50,
+    alignItems: 'center',
+    marginBottom: 2,
+    width: '100%',
 
     padding: 15,
- 
-
-  }
+  },
 });
