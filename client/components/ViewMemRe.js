60233f3f2ccb818dba7130e03f68c92e063a7eac
ahaha <101982272+Git4RJS@users.noreply.github.com>
2023-06-20 10:57:32 +0700

    Create ViewMemRe.js

---
+++ client/components/ViewMemRe.js
@@ -0,0 +1,182 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import UserInfo from "./UserInfo";
+import { SwipeListView } from 'react-native-swipe-list-view';
+
+let dt = [
+  { id: '122', name: 'han' },
+  { id: '123', name: 'ban' },
+];
+
+export default function Basic(props) {
+  let a = dt.length;
+  const [listData1, setListData1] = useState(
+    Array(a)
+      .fill('')
+      .map((_, i) => ({ key: `${i}`, id: dt[i].id, name: dt[i].name }))
+  );
+  const [listData2, setListData2] = useState(
+    Array(a)
+      .fill('')
+      .map((_, i) => ({ key: `${i}`, id: dt[i].id, filename: dt[i].filename }))
+  );
+
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
+
+  const onRowDidOpen = (rowKey) => {
+    console.log('This row opened', rowKey);
+  };
+  const renderItem = (data) => (
+    <TouchableHighlight
+      onPress={() => console.log('You touched me')}
+      style={styles.rowFront}
+      underlayColor={'#AAA'}>
+      <View>
+        <Text style={styles.txt}>Name: {data.item.name}</Text>
+        <Text style={styles.txt}>ID: {data.item.id}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+  const viewDetails = (data) => {
+     const id=data.item.id;
+     console.log("ID=",data.item.id);
+     <UserInfo/>;
+     props.navigation.navigate('User Info',{
+            userid: data.item.id,}
+     );
+
+  };
+
+  const save = async (uri, filename, mimetype) => {
+    if (Platform.OS === 'android') {
+      const permissions =
+        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(uri, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+        await FileSystem.StorageAccessFramework.createFileAsync(
+          permissions.directoryUri,
+          filename,
+          mimetype
+        )
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, base64, {
+              encoding: FileSystem.EncodingType.Base64,
+            });
+          })
+          .catch((e) => console.log(e));
+      } else {
+        shareAsync(uri);
+      }
+    } else {
+      shareAsync(uri);
+    }
+  };
+
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => viewDetails(data, rowMap)}>
+        <Text style={styles.backTextWhite}>View Details</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Parents</Text>
+
+        <SwipeListView
+          data={listData1}
+          renderItem={renderItem}
+          renderHiddenItem={renderHiddenItem}
+          leftOpenValue={75}
+          rightOpenValue={-150}
+          previewRowKey={'0'}
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
+          onRowDidOpen={onRowDidOpen}
+        />
+      </View>
+      <View>
+        <Text style={styles.title}>Children</Text>
+
+        <SwipeListView
+          data={listData2}
+          renderItem={renderItem}
+          renderHiddenItem={renderHiddenItem}
+          leftOpenValue={75}
+          rightOpenValue={-150}
+          previewRowKey={'0'}
+          previewOpenValue={-40}
+          previewOpenDelay={3000}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  txt: {
+    color: '#0039a6',
+    fontSize: 15,
+  },
+  rowFront: {
+     alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#0039a6',
+    justifyContent: 'center',
+    height: 80,
+  },
+  title: {
+    marginTop: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 25,
+    color: '#0039a6',
+  },
+
+  rowBack: {
+    alignItems: 'center',
+
+    flex: 1,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 155,
+  },
+
+  backRightBtnRight: {
+    backgroundColor: '#0039a6',
+    right: 0,
+  },
+});
